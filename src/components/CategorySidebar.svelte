1cd0e8c8ea23c6df73178135edddcfd0283f36ce
tommy4377 <129632197+tommy4377@users.noreply.github.com>
2026-01-17 18:42:38 +0100

    g

--- src/components/CategorySidebar.svelte
+++ src/components/CategorySidebar.svelte
@@ -63,7 +63,7 @@
 <style>
     .sidebar {
         width: 240px;
-        background: var(--layer-nav);
+        background: transparent;
         border-right: 1px solid var(--border-color);
         padding: 16px 0;
         display: flex;
